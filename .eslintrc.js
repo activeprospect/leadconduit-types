dbc59032bb4bd47d9e4ea72c30ad67efa6d57d4c
Blake Sager <blake.sager@activeprospect.com>
2020-02-11 08:57:01 -0600

    Update dot files

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "mocha": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+    "extends": "standard",
+    "rules": {
+      "semi": [2, "always"],
+      "no-extra-semi": 2,
+      "no-useless-return": "error",
+      "prefer-arrow-callback": "error",
+      "prefer-const": "error"      
+    }
+}
