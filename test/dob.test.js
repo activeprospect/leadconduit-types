0a7a3133cd3573563150ad8a3a0f1efdf594cd23
jgrayson676 <jonathan@activeprospect.com>
2022-09-01 10:58:37 -0500

    add dob type (#149)

---
+++ test/dob.test.js
@@ -0,0 +1,63 @@
+const { assert } = require('chai');
+const dob = require('../lib/types/dob');
+const timefreeze = require('timefreeze');
+
+describe('DOB', function () {
+  before(function () {
+    // June 18, 2022
+    timefreeze.freeze(new Date(2022,5,18));
+  });
+  after(function () {
+    timefreeze.reset();
+  });
+
+  it('should return a date object', function () {
+    const parsed = dob.parse('06/02/2014');
+    assert.instanceOf(parsed, Date);
+    assert.equal(parsed.toISOString(), '2014-06-02T00:00:00.000Z')
+  });
+
+  it('should have string value', function () {
+    const parsed = dob.parse('06/02/2014');
+    assert.equal(parsed.toString(), '2014-06-02');
+    assert.equal(parsed.valueOf(), '2014-06-02');
+  });
+
+  it('should have age', function () {
+    assert.equal(dob.parse('06/02/2014').age, 8);
+  });
+
+  it('should have year', function () {
+    assert.equal(dob.parse('06/02/2014').year, 2014);
+  });
+
+  it('should have raw value', function () {
+    assert.equal(dob.parse('06/02/2014').raw, '06/02/2014');
+  });
+
+  it('should be valid', function () {
+    assert.isTrue(dob.parse('06/02/2014').valid);
+  });
+
+  it('should not parse garbage', function () {
+    const parsed = dob.parse('garbage');
+    assert.equal(parsed.toString(), 'garbage');
+    assert.equal(parsed.valueOf(), 'garbage');
+    assert.equal(parsed.raw, 'garbage');
+    assert.isFalse(parsed.valid);
+  });
+
+  it('should handle parsing a parsed date', function () {
+    const parsed = dob.parse(dob.parse('Mon Jun 02 2014'));
+    assert.instanceOf(parsed, Date);
+    assert.equal(parsed.toString(), '2014-06-02');
+    assert.equal(parsed.valueOf(), '2014-06-02');
+    assert.equal(parsed.raw, 'Mon Jun 02 2014');
+    assert.equal(parsed.year, 2014);
+    assert.equal(parsed.age, 8);
+  });
+
+  it('should have examples', function () {
+    assert(dob.examples.length);
+  });
+});
