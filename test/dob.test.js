8d34a4554efaf1cdb03c2f9c2a505b6bf88c4c8b
John Bench <johnrb2@users.noreply.github.com>
2022-12-09 11:53:56 -0500

    sc-46668/fix-dob-type-examples (#152)
    * parse and normalize dob examples
    
    * fix issue with age calculation and update tests
    
    * update date comment
    
    * update age calculation
    
    * add moment-timezone
    
    * ensure same timezone for age calculation
    
    * revising calculation again
    
    * validate age calc for 102 years
    
    * fix calculation for all years
    
    * change duration based on whether birthday has happened yet or not
    
    * clean up tests
    
    * cleanup code a bit, modify age calculation
    
    * remove moment-timezone not needed for utc
    
    * remove moment-timezone
    
    * remove moment-timezone, add 144 minutes between today and birthday

--- test/dob.test.js
+++ test/dob.test.js
@@ -1,11 +1,13 @@
 const { assert } = require('chai');
 const dob = require('../lib/types/dob');
 const timefreeze = require('timefreeze');
+const moment = require('moment');
+const MAX_HUMAN_LIFE_SPAN_YEARS = 121;
 
 describe('DOB', function () {
   before(function () {
-    // June 18, 2022
-    timefreeze.freeze(new Date(2022,5,18));
+    // June 2, 2022
+    timefreeze.freeze(new Date(2022,5,2));
   });
   after(function () {
     timefreeze.reset();
@@ -23,8 +25,27 @@ describe('DOB', function () {
     assert.equal(parsed.valueOf(), '2014-06-02');
   });
 
-  it('should have age', function () {
-    assert.equal(dob.parse('06/02/2014').age, 8);
+  it('should get decimal right on birthday', function () {
+    for (let i = MAX_HUMAN_LIFE_SPAN_YEARS; i--;) {
+      const dateOfBirth = moment.utc().subtract(i, 'years').format('YYYY-MM-DD');
+      assert.equal(dob.parse(dateOfBirth).age, i);
+    }
+  });
+
+  it('should get decimal right day before birthday', function () {
+    // to prevent selecting a negative birthday start at 1.
+    for (let i = 1; i < MAX_HUMAN_LIFE_SPAN_YEARS; i++) {
+      const dateOfBirth = moment.utc().add(1, 'day').subtract(i, 'years').format('YYYY-MM-DD');
+      assert.equal(dob.parse(dateOfBirth).age, i - 0.1);
+    }
+  });
+
+  it('should get the decimal right for date after birthday', function () {
+    const ONE_TENTH_YEAR_IN_DAYS = 37;
+    for (let i = MAX_HUMAN_LIFE_SPAN_YEARS; i--;) {
+      const dateOfBirth = moment.utc().subtract(ONE_TENTH_YEAR_IN_DAYS, 'day').subtract(i, 'years').format('YYYY-MM-DD');
+      assert.equal(dob.parse(dateOfBirth).age, i + 0.1);
+    }
   });
 
   it('should have year', function () {
