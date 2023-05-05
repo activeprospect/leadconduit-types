75133c05c6a336f9af67bbbe2ad950ba5efc2107
johnrb2 <john.bench@activeprospect.com>
2023-05-05 17:13:38 -0400

    add tests for new functions

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,7 @@ const { assert } = require('chai');
 const fs = require('fs');
 const path = require('path');
 const index = require('../lib');
+const dob = require('../lib/types/dob');
 
 describe('Index', function () {
   it('should list type names', function () {
@@ -34,5 +35,30 @@ describe('Index', function () {
     };
     const objClone = index.clone(obj);
     assert.deepEqual(objClone, obj);
+  });
+
+  it('returns true for dob type', function () {
+    // tests shouldHide()
+    const normalizedDob = index.normalize(dob.parse('01-01-2000'));
+    assert.isTrue(index.shouldHide(normalizedDob));
+  });
+
+  it('returns false for non-hideable type', function () {
+    // tests shouldHide()
+    const normalizedString = index.normalize('string');
+    assert.isFalse(index.shouldHide(normalizedString));
+  });
+
+  it('partially hide dob data', function () {
+    // tests getHideableType() and hide()
+    const normalizedDob = index.normalize(dob.parse('01-01-2000'));
+    const hidden = index.hide(normalizedDob);
+    assert.equal(hidden.raw, '**/**/2000');
+    assert.equal(hidden.normal, '**/**/2000');
+    assert.equal(hidden.year, 2000);
+    // check that age is still a number
+    assert.isTrue(Number.isFinite(Number.parseFloat(hidden.age)));
+    assert.isTrue(hidden.masked);
+    assert.isTrue(hidden.valid);
   });
 });
