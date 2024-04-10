779ca94ca7d97e2553b092ab6585b34dbbf8c286
Henrique Laki <henrique.laki@activeprospect.com>
2024-04-10 15:44:57 -0300

    Add string validation to first_name

--- test/first-name.test.js
+++ test/first-name.test.js
@@ -1,12 +1,25 @@
 const { assert } = require('chai');
 const firstName = require('../lib/types/first_name');
 
-describe('First Name', function () {
+describe.only('First Name', function () {
   it('should be no-op', function () {
     assert.equal(firstName.parse('Alex'), 'Alex');
   });
 
   it('should have examples', function () {
     assert(firstName.examples.length);
+  });
+
+  it('should handle non-string garbage', function () {
+    let parsed, stringified;
+    const invokeStringify = () => {
+      parsed = firstName.parse({ foo: 42 });
+      stringified = JSON.stringify(parsed);
+    };
+    assert.doesNotThrow(invokeStringify);
+
+    assert.equal(stringified, '"[object Object]"');
+    assert.equal(parsed.raw, '[object Object]');
+    assert.isFalse(parsed.valid);
   });
 });
