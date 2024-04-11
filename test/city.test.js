dfa0dbf595b5196d8f5de4fd6203bb83e193af58
Henrique Laki <henrique.laki@activeprospect.com>
2024-04-11 15:06:40 -0300

    Add string validation and 'is valid/invalid format' operators to last_name and city

--- test/city.test.js
+++ test/city.test.js
@@ -9,4 +9,17 @@ describe('City', function () {
   it('should have examples', function () {
     assert(city.examples.length);
   });
+
+  it('should handle non-string garbage', function () {
+    let parsed, stringified;
+    const invokeStringify = () => {
+      parsed = city.parse({ foo: 42 });
+      stringified = JSON.stringify(parsed);
+    };
+    assert.doesNotThrow(invokeStringify);
+
+    assert.equal(stringified, '"[object Object]"');
+    assert.equal(parsed.raw, '[object Object]');
+    assert.isFalse(parsed.valid);
+  });
 });
